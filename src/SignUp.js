63a87b89be63dad5283b675e31a56c415adea60d
Farzad Solaimani <farzadsolaimanii@gmail.com>
2023-05-31 10:17:47 +0400

    Basic things has been written

---
+++ src/SignUp.js
@@ -0,0 +1,84 @@
+import React, { useState } from "react";
+
+const SignUp = () => {
+  const [data, setData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+    isAccepted: false,
+  });
+
+  const changeHandler = (event) => {
+    if (event.target.name == "isAccepted") {
+      setData({ ...data, [event.target.name]: event.target.checked });
+    } else {
+      setData({ ...data, [event.target.name]: event.target.value });
+    }
+
+    console.log(data);
+  };
+
+  return (
+    <div>
+      <h3> Sign up </h3>
+      <form>
+        <div>
+          <label> name </label>
+          <input
+            type="text"
+            name="name"
+            value={data.name}
+            onChange={changeHandler}
+          />
+        </div>
+
+        <div>
+          <label> Email </label>
+          <input
+            type="email"
+            name="email"
+            value={data.email}
+            onChange={changeHandler}
+          />
+        </div>
+
+        <div>
+          <label> Password </label>
+          <input
+            type="password"
+            name="password"
+            value={data.password}
+            onChange={changeHandler}
+          />
+        </div>
+
+        <div>
+          <label> Confirm Password </label>
+          <input
+            type="password"
+            name="confirmPassword"
+            value={data.confirmPassword}
+            onChange={changeHandler}
+          />
+        </div>
+
+        <div>
+          <label> I accept terms of privacy policy </label>
+          <input
+            type="checkbox"
+            name="isAccepted"
+            value={data.isAccepted}
+            onChange={changeHandler}
+          />
+        </div>
+
+        <div>
+          <a href="#"> Login </a>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default SignUp;
